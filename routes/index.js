5b7bb946a5e055441ca3aab89e4649c7634fa03c
klgibsonjr <klgibsonjr@gmail.com>
2023-03-07 20:36:57 -0800

    created userRoutes

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = requrie('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  return res.send('Wrong route!');
+});
+
+module.exports = router;
