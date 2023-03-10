850d3d485b84096455f7a886a7cfb6adeb677865
klgibsonjr <klgibsonjr@gmail.com>
2023-03-09 18:32:43 -0800

    started thoughts controller, able to get routes working

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,11 +1,26 @@
 const { Thought, User } = require('../models');
 
 module.exports = {
-  getThought() {},
+  getThought(req, res) {
+    Thought.find()
+      .then((user) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
 
-  getSingleThought() {},
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
-  createThought() {},
+  createThought(req, res) {
+    Thought.create(req.body).then((dbThoughtData) => res.json(dbThoughtData));
+  },
 
   updateThought() {},
 
