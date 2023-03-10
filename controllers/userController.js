2ddb2a1c6137dd0f82482841d229cd720a70e1dd
klgibsonjr <klgibsonjr@gmail.com>
2023-03-09 16:52:39 -0800

    worked on user controller file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,43 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
-  getUsers() {},
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
 
-  getSingleUser() {},
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .populate('Thought')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No use with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
-  createUser() {},
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
 
-  updateUser() {},
+  updateUser(req, res) {
+    User.findByIdAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(400).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 
   deleteUser() {},
 
